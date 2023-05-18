b84ec50829c88fa3c6309f25f49052273972a169
Samuel Ferragne <xavier.racine2018@gmail.com>
2023-05-18 15:25:32 -0400

    fix routes stages et packages

---
+++ backend/models/http-erreur.js
@@ -0,0 +1,8 @@
+class HttpErreur extends Error {
+  constructor(message, codeErreur) {
+    super(message);
+    this.code = codeErreur;
+  }
+}
+
+module.exports = HttpErreur;
--- backend/routes/stages-routes.js
+++ backend/routes/stages-routes.js
@@ -3,12 +3,10 @@ const express = require("express");
 const controllerStage = require("../controllers/stages-controllers");
 const router = express.Router();
 
-router.get("/:stageId", controllerStage.getCoursById);
+router.get("/:stageId", controllerStage.getStageById);
 
-router.post("/", controllerStage.creerCours);
+router.post("/", controllerStage.creerStage);
 
-router.patch("/:stageId", controllerStage.updateCours);
-
-router.delete("/:stageId", controllerStage.supprimerCours);
+router.delete("/:stageId", controllerStage.supprimerStage);
 
 module.exports = router;
